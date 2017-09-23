8efde64c4d5ac7a5c1a6f82fa9a0be5e528a6015
Pranav <pranav.ppanch@gmail.com>
2017-09-23 16:48:14 +0530

    Auto Complete Settings Added

--- latex-settings.el
+++
@@ -1,9 +0,0 @@
-;For Tex
-(require 'yasnippet)
-(require 'tex)
-(add-hook 'LaTeX-mode-hook (lambda ()
-  (TeX-global-PDF-mode t)
-  (auto-complete-mode t)
-  (flyspell-mode t)
-  (yas-global-mode 1)
-    ))
