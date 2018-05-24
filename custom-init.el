5900d44e890b87501820560bb80199ff399c8387
Pranav <pranav.ppanch@gmail.com>
2018-05-24 18:09:28 +0530

    TeX view

--- custom-init.el
+++ custom-init.el
@@ -4,6 +4,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi has-no-display-manager)
+      "dvi2tty")
+     ((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "Okular")
+     (output-html "xdg-open"))))
  '(custom-enabled-themes (quote (misterioso)))
  '(inhibit-startup-screen t)
  '(org-agenda-dim-blocked-tasks t)
@@ -38,11 +47,11 @@
  '(org-log-done (quote note))
  '(org-log-into-drawer t)
  '(org-log-reschedule (quote note))
- '(org-log-states-order-reversed nil)
+ '(org-log-states-order-reversed t)
  '(org-refile-allow-creating-parent-nodes (quote confirm))
  '(org-refile-targets (quote ((org-agenda-files :level . 1))))
  '(org-refile-use-outline-path (quote file))
- '(preview-TeX-style-dir "" t)
+ '(preview-TeX-style-dir "")
  '(preview-auto-cache-preamble t)
  '(preview-fast-conversion nil)
  '(show-paren-mode t))
